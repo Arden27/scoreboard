457c69c9082a526e18c8c1a0f377b8510e66ba0d
Artem Furman <a.furman@andersenlab.com>
2024-10-16 13:52:06 +0200

    FEATURE: add score update

--- src/match.ts
+++ src/match.ts
@@ -1,3 +1,5 @@
+import { ERROR_NEGATIVE_SCORE } from "./constants";
+
 export class Match {
   homeTeam: string;
   awayTeam: string;
@@ -11,5 +13,14 @@ export class Match {
     this.homeScore = 0;
     this.awayScore = 0;
     this.startTime = Date.now();
+  }
+
+  updateScore(newHomeScore: number, newAwayScore: number) {
+    if (newHomeScore < 0 || newAwayScore < 0) {
+      throw new Error(ERROR_NEGATIVE_SCORE);
+    }
+    
+    this.homeScore = newHomeScore;
+    this.awayScore = newAwayScore;
   }
 }
