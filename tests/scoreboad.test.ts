a36a8ee6a8b70b972a54153e2bf9bfb5a840cafc
Artem Furman <a.furman@andersenlab.com>
2024-10-17 14:52:50 +0200

    TEST: add basic failing tests for starting match and summary

--- tests/scoreboad.test.ts
+++ tests/scoreboad.test.ts
@@ -1,7 +1,28 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, beforeEach } from "vitest";
 
-describe('initial test', () => {
-  it('should pass', () => {
-    expect(true).toBe(true);
+import { Scoreboard } from "../src/scoreboard";
+describe("Scoreboard Class", () => {
+  let scoreboard: Scoreboard;
+  let summary: string[];
+
+  beforeEach(() => {
+    scoreboard = new Scoreboard();
+    summary = [];
+  });
+
+  it("should start a new match and add it to the scoreboard", () => {
+    scoreboard.startMatch("Australia", "Cuba");
+    summary = scoreboard.getSummary();
+    expect(summary).toEqual(["1. Australia 0 - Cuba 0"]);
+  });
+
+  it("should be sorted by date", async () => {
+    scoreboard.startMatch("Australia", "Cuba");
+    scoreboard.startMatch("Mongolia", "Canada");
+    summary = scoreboard.getSummary();
+    expect(summary).toEqual([
+      "1. Mongolia 0 - Canada 0",
+      "2. Australia 0 - Cuba 0",
+    ]);
   });
 });
