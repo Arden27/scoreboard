b98bc407139be0484da800d5f69a291d956fe462
Artem Furman <a.furman@andersenlab.com>
2024-10-23 01:09:31 +0200

    REFACTOR: add key parts comments

--- src/scoreboard.ts
+++ src/scoreboard.ts
@@ -6,9 +6,9 @@ import {
 } from "./constants";
 
 export class Scoreboard {
-  matches: Map<string, Match> = new Map();
-  sortedMatches: Match[] = [];
-  activeTeams: Set<string> = new Set();
+  matches: Map<string, Match> = new Map(); // Map to store matches with a unique key
+  sortedMatches: Match[] = []; // Array to maintain sorted matches for summary
+  activeTeams: Set<string> = new Set(); // Set to track active teams to prevent duplicate participation
 
   startMatch(homeTeam: string, awayTeam: string) {
     const { homeTeamNormalized, awayTeamNormalized } = this.getNormalizedTeams(
@@ -27,9 +27,10 @@ export class Scoreboard {
     );
     const match = new Match(homeTeam, awayTeam);
     const insertIndex = this.findInsertionIndex(match);
-
+     // Add the match to the matches map and sortedMatches array
     this.matches.set(matchKey, match);
     this.sortedMatches.splice(insertIndex, 0, match);
+    // Mark both teams as active
     this.activeTeams.add(homeTeamNormalized);
     this.activeTeams.add(awayTeamNormalized);
   }
@@ -52,6 +53,7 @@ export class Scoreboard {
 
     if (match) {
       match.updateScore(newHomeScore, newAwayScore);
+      // Remove and re-insert the match to maintain correct order
       const removeIndex = this.sortedMatches.indexOf(match);
       this.sortedMatches.splice(removeIndex, 1);
       const insertIndex = this.findInsertionIndex(match);
@@ -72,9 +74,10 @@ export class Scoreboard {
 
     if (match) {
       const removeIndex = this.sortedMatches.indexOf(match);
-
+      // Remove the match from the sortedMatches array and matches map
       this.sortedMatches.splice(removeIndex, 1);
       this.matches.delete(matchKey);
+      // Remove teams from activeTeams set
       this.activeTeams.delete(homeTeamNormalized);
       this.activeTeams.delete(awayTeamNormalized);
     } else throw new Error(ERROR_NO_MATCH);
@@ -89,6 +92,7 @@ export class Scoreboard {
     );
   }
 
+  // binary search to find the insertion index
   private findInsertionIndex(newMatch: Match): number {
     const totalScore = newMatch.homeScore + newMatch.awayScore;
     let low = 0;
@@ -133,6 +137,7 @@ export class Scoreboard {
     return homeTeamNormalized === awayTeamNormalized;
   }
 
+  // checks if any team is already in play
   private isPlaying(homeTeamNormalized: string, awayTeamNormalized: string) {
     return (
       this.activeTeams.has(homeTeamNormalized) ||
